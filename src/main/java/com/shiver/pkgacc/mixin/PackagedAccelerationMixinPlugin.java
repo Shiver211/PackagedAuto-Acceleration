387841ad8955acc6dd200ec17695e97e6b166347
Shiver211 <2863677073@qq.com>
2026-07-03 20:46:59 +0800

    欸我的文件哪去了

---
+++ src/main/java/com/shiver/pkgacc/mixin/PackagedAccelerationMixinPlugin.java
@@ -0,0 +1,45 @@
+package com.shiver.pkgacc.mixin;
+
+import net.minecraft.launchwrapper.Launch;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+public class PackagedAccelerationMixinPlugin implements IMixinConfigPlugin {
+
+    @Override
+    public void onLoad(String mixinPackage) {}
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        try {
+            return Launch.classLoader.getClassBytes(targetClassName) != null;
+        }
+        catch(IOException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+}
